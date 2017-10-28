e2f70001e6f9e5b2bc55c6c0fc7dfc1b8dcb5d8e
Brandon <bme54950@gmail.com>
2017-10-27 21:31:38 -0500

    Created accessControl helpers for ease of use

---
+++ local_api/helpers/accessControl.py
@@ -0,0 +1,18 @@
+from local_objects.required_objects import Session
+
+def getUserBySession(sessionID, dbSession, ip):
+	"""Returns userID given a @param1: SessionID, and @param2: databaseSession
+	If no valid session, returns None"""
+	try:
+		sessionObject = dbSession.query(Session).filter(Session.id == sessionID).one()
+		if (ip == sessionObject.addressIssued):
+			return sessionObject.userID
+		else:
+			print("Possible session hijack. IP Mismatch (%s != %s)"%(ip, sessionObject.addressIssued))
+			return None
+	except:
+		print("Encountered Error in getUserBySession")
+		return None
+
+def getPeerIP(local_node):
+	return local_node.transport.getPeer().host
